a08efcd89c25fbbd1f52d7b96bad70d5ad04f7c2
woodchuck28 <charles.daab@gmail.com>
2022-04-26 04:48:53 -0400

    removed carousel

--- frontend/src/containers/APHuman.js
+++ frontend/src/containers/APHuman.js
@@ -11,33 +11,9 @@ import {
 export default function APHuman() {
     return(
         
-            <MDBCarousel showIndicators showControls fade className={styles.Carousel}>
-            <MDBCarouselInner>
-                <MDBCarouselItem className='active'>
-                <MDBCarouselElement src='https://mdbootstrap.com/img/Photos/Slides/img%20(15).webp' alt='...' />
-                <MDBCarouselCaption>
-                    <h5>Welcome to AP Human Geography</h5>
-                    
-                </MDBCarouselCaption>
-                </MDBCarouselItem>
-
-                <MDBCarouselItem>
-                <MDBCarouselElement src='https://mdbootstrap.com/img/Photos/Slides/img%20(22).webp' alt='...' />
-                <MDBCarouselCaption>
-                    <h5>Second slide label</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </MDBCarouselCaption>
-                </MDBCarouselItem>
-
-                <MDBCarouselItem>
-                <MDBCarouselElement src='https://mdbootstrap.com/img/Photos/Slides/img%20(23).webp' alt='...' />
-                <MDBCarouselCaption>
-                    <h5>Third slide label</h5>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </MDBCarouselCaption>
-                </MDBCarouselItem>
-            </MDBCarouselInner>
-            </MDBCarousel>
+            <div>
+                
+            </div>
 
         
 
