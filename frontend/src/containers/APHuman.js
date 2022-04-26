8c840740280ec5ec4ffdc32861945789b4d4f63d
woodchuck28 <charles.daab@gmail.com>
2022-04-26 04:55:28 -0400

    removed unused

--- frontend/src/containers/APHuman.js
+++ frontend/src/containers/APHuman.js
@@ -1,18 +1,11 @@
 import React from 'react'
-import {
-    MDBCarousel,
-    MDBCarouselInner,
-    MDBCarouselItem,
-    MDBCarouselElement,
-    MDBCarouselCaption,
-  } from 'mdb-react-ui-kit';
-  import styles from './mystyles.module.css'
+
 
 export default function APHuman() {
     return(
         
             <div>
-                
+
             </div>
 
         
